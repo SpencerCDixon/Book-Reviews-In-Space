5d55ecb32af95ea8929ed92c6fbb39b4e2d55c89
Spencer Dixon <spencercdixon@gmail.com>
2014-12-20 15:48:17 -0500

    Wrote model tests for book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Book, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:title).when('Title', 'Seasons of Life', 'a') }
+  it { should_not have_valid(:title).when('', nil) }
+
+  it { should have_valid(:description).when('This is a description of a book') }
+  it { should_not have_valid(:description).when('', nil) }
+
+  it { should have_valid(:URL).when('http://www.example.com', 'www.example.com') }
+  it { should_not have_valid(:URL).when('', nil, 'asdfkljsdf') }
 end
