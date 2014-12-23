648979d54292745b3c8d4421642b7f6230843ac3
Spencer Dixon <spencercdixon@gmail.com>
2014-12-23 17:12:05 -0500

    Wrote unit tests for reviews and made them pass

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Review, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:description).when('Title', 'Seasons of Life', 'a') }
+  it { should_not have_valid(:description).when('', nil) }
 end
